bd46d7bee491cd497a8e3de0cd21640584946a6f
Nirmal <nirmalr2512@gmail.com>
2022-04-21 15:24:42 +0530

    Null Safety Added

--- lib/loading_skeleton.dart
+++ lib/loading_skeleton.dart
@@ -36,11 +36,11 @@ extension AnimationEndExtension on AnimationEnd {
 class LoadingSkeleton extends StatefulWidget {
   final double width;
   final double height;
-  final EdgeInsetsGeometry margin;
+  final EdgeInsetsGeometry? margin;
   final List<Color> colors;
   final AnimationEnd animationEnd;
   final int animationDuration;
-  final Widget child;
+  final Widget? child;
 
   /// This method constructs the LoadingSkeleton widget
   ///
@@ -56,16 +56,14 @@ class LoadingSkeleton extends StatefulWidget {
   ///
   /// Param [child] can be used to get child widgets inside of loading skeleton
   LoadingSkeleton({
-    @required this.width,
-    @required this.height,
+    required this.width,
+    required this.height,
     this.margin,
     this.colors = const [Colors.black12, Colors.black26, Colors.black12],
     this.animationEnd = AnimationEnd.NORMAL,
     this.animationDuration = 3000,
     this.child,
-  })  : assert(width != null),
-        assert(height != null),
-        assert(colors.length > 1);
+  }) : assert(colors.length > 1);
 
   /// Method that create loading skeleton state
   @override
@@ -75,10 +73,10 @@ class LoadingSkeleton extends StatefulWidget {
 class _LoadingSkeletonState extends State<LoadingSkeleton>
     with SingleTickerProviderStateMixin {
   /// Field responsable for controlling the animation
-  AnimationController _controller;
+  late AnimationController _controller;
 
   /// Field responsable for handle animation values
-  Animation gradientPosition;
+  late Animation gradientPosition;
 
   /// Method that initializes the widget and the animation
   @override
